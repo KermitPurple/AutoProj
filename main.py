84d89562c60aac59f2faef09044a5ad3a083d531
Shane McDonough <shanemcdman@gmail.com>
2020-02-20 14:02:21 -0800

    Initial

---
+++ main.py
@@ -0,0 +1,49 @@
+from os import system
+from msvcrt import getch
+
+def makeNew():
+    print("1) New C++ Project")
+    print("2) New Python Project")
+    print("3) New Java Project")
+    print("4) New web Project")
+    print("5) New rust Project")
+    print("6) Cancel")
+    while 1:
+        try:
+            choice = int(getch().decode("utf-8"))
+            if choice > 6:
+                raise
+            break
+        except:
+            print("enter valid number")
+    if choice == 1:
+        makeNewC()
+    elif choice == 2:
+        makeNewPython()
+    elif choice == 3:
+        makeNewJava()
+    elif choice == 4:
+        makeNewWeb()
+    elif choice == 5:
+        makeNewRust()
+
+
+def makeNewC():
+    pass
+
+def makeNewPython():
+    pass
+
+def makeNewJava():
+    pass
+
+def makeNewWeb():
+    pass
+
+def makeNewrust():
+    pass
+
+def makeNewRust():
+    pass
+
+makeNew()
