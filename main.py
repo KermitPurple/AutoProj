7d26dc5f59f870901e564fe5b172021236ac7637
Shane McDonough <shanemcdman@gmail.com>
2020-04-05 20:17:41 -0700

    Format with tabs for rainbow

--- main.py
+++ main.py
@@ -3,122 +3,121 @@ from msvcrt import getch
 from sys import argv
 
 def YesOrNo():
-    print("(Y/N)")
-    while 1:
-        try:
-            choice = getch().decode("utf-8").lower()
-            if choice == 'y':
-                return True
-            elif choice == 'n':
-                return False
-            else:
-                raise
-        except:
-            print("Please Enter y or n")
+	print("(Y/N)")
+	while 1:
+		try:
+			choice = getch().decode("utf-8").lower()
+			if choice == 'y':
+				return True
+			elif choice == 'n':
+				return False
+			else:
+				raise
+		except:
+			print("Please Enter y or n")
 
 def makeNew():
-    print("1) New C++ Project")
-    print("2) New Python Project")
-    print("3) New Java Project")
-    print("4) New web Project")
-    print("5) New rust Project")
-    print("6) Cancel")
-    while 1:
-        try:
-            choice = int(getch().decode("utf-8"))
-            if choice > 6:
-                raise
-            break
-        except:
-            print("enter valid number")
-    if choice != 6:
-        name = input("Enter Name: ")
-        if choice == 1:
-            makeNewC(name)
-        elif choice == 2:
-            makeNewPython(name)
-        elif choice == 3:
-            makeNewJava(name)
-        elif choice == 4:
-            makeNewWeb(name)
-        elif choice == 5:
-            makeNewRust(name)
+	print("1) New C++ Project")
+	print("2) New Python Project")
+	print("3) New Java Project")
+	print("4) New web Project")
+	print("5) New rust Project")
+	print("6) Cancel")
+	while 1:
+		try:
+			choice = int(getch().decode("utf-8"))
+			if choice > 6:
+				raise
+			break
+		except:
+			print("enter valid number")
+	if choice != 6:
+		name = input("Enter Name: ")
+		if choice == 1:
+			makeNewC(name)
+		elif choice == 2:
+			makeNewPython(name)
+		elif choice == 3:
+			makeNewJava(name)
+		elif choice == 4:
+			makeNewWeb(name)
+		elif choice == 5:
+			makeNewRust(name)
 
 def makeNewC(name):
-    print("Is this a personal project", end="")
-    personal = YesOrNo()
-    path = ""
-    if personal:
-        path = "C:\\users\\shane\\dropbox\\desktop\\coding\\c++\\"+name
-    else:
-        path = "C:\\users\\shane\\dropbox\\desktop\\school\\senior year\\semester 2\\cs\\"+name
-    system("md " + "\"" + path + "\"")
-    chdir(path)
-    system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\makefile\" .")
-    system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\src\" .")
-    system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\bin\" .")
-    system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\include\" .")
-    system("gvim src\\main.cpp")
+	print("Is this a personal project", end="")
+	personal = YesOrNo()
+	path = ""
+	if personal:
+		path = "C:\\users\\shane\\dropbox\\desktop\\coding\\c++\\"+name
+	else:
+		path = "C:\\users\\shane\\dropbox\\desktop\\school\\senior year\\semester 2\\cs\\"+name
+	system("md " + "\"" + path + "\"")
+	chdir(path)
+	system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\makefile\" .")
+	system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\src\" .")
+	system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\bin\" .")
+	system("cp -r \"C:\\users\\shane\\dropbox\\desktop\\coding\\defaults\\c++\\include\" .")
+	system("gvim src\\main.cpp")
 
 def makeNewPython(name):
-    print("Is this a discord bot?", end="")
-    bot = YesOrNo()
-    if bot:
-        path = "C:\\users\\shane\\dropbox\\desktop\\coding\\python\\discordbots\\"+name
-    else:
-        path = "C:\\users\\shane\\dropbox\\desktop\\coding\\python\\"+name
-    system("md "+path)
-    chdir(path)
-    system("gvim main.py")
+	print("Is this a discord bot?", end="")
+	bot = YesOrNo()
+	if bot:
+		path = "C:\\users\\shane\\dropbox\\desktop\\coding\\python\\discordbots\\"+name
+	else:
+		path = "C:\\users\\shane\\dropbox\\desktop\\coding\\python\\"+name
+	system("md "+path)
+	chdir(path)
+	system("gvim main.py")
 
 def makeNewJava(name):
-    print("not implimented yet")
+	print("not implimented yet")
 
 def makeNewWeb(name):
-    print("Is this plain javascript", end="")
-    bot = YesOrNo()
-    if bot:
-        path = "C:\\users\\shane\\dropbox\\desktop\\coding\\web\\JustJS\\"+name
-    else:
-        path = "C:\\users\\shane\\dropbox\\desktop\\coding\\web\\"+name
-    system("md "+path)
-    chdir(path)
-    system("cp C:\\Users\\Shane\\Dropbox\\Desktop\\Coding\\defaults\\web\\index.html .")
-    system("touch styles.css")
-    system("touch sketch.js")
-    system("gvim")
+	print("Is this plain javascript", end="")
+	bot = YesOrNo()
+	if bot:
+		path = "C:\\users\\shane\\dropbox\\desktop\\coding\\web\\JustJS\\"+name
+	else:
+		path = "C:\\users\\shane\\dropbox\\desktop\\coding\\web\\"+name
+	system("md "+path)
+	chdir(path)
+	system("cp C:\\Users\\Shane\\Dropbox\\Desktop\\Coding\\defaults\\web\\index.html .")
+	system("touch styles.css")
+	system("touch sketch.js")
+	system("gvim")
 
 def makeNewRust(name):
-    print("not implimented yet")
+	print("not implimented yet")
 
 def parseArgv():
-    try:
-        projType, projName = argv[1:3]
-    except:
-        projType = argv[1]
-        projName = input("Enter Name: ")
-    cppList = ['c', 'cpp', 'c++']
-    pythonList = ['p', 'py', 'pyth', 'python']
-    javaList = ['j', 'jv', 'java']
-    webList = ['w', 'web', 'html', 'css', 'js', 'javascript']
-    rustList = ['r', 'rs', 'rust']
-    if projType.lower() in cppList:
-        makeNewC(projName)
-    elif projType.lower() in pythonList:
-        makeNewPython(projName)
-    elif projType.lower() in javaList:
-        makeNewJava(projName)
-    elif projType.lower() in webList:
-        makeNewWeb(projName)
-    elif projType.lower() in rustList:
-        makeNewRust(projName)
-    else:
-        print("ERROR: A project type with that name does not exist")
+	try:
+		projType, projName = argv[1:3]
+	except:
+		projType = argv[1]
+		projName = input("Enter Name: ")
+	cppList = ['c', 'cpp', 'c++']
+	pythonList = ['p', 'py', 'pyth', 'python']
+	javaList = ['j', 'jv', 'java']
+	webList = ['w', 'web', 'html', 'css', 'js', 'javascript']
+	rustList = ['r', 'rs', 'rust']
+	if projType.lower() in cppList:
+		makeNewC(projName)
+	elif projType.lower() in pythonList:
+		makeNewPython(projName)
+	elif projType.lower() in javaList:
+		makeNewJava(projName)
+	elif projType.lower() in webList:
+		makeNewWeb(projName)
+	elif projType.lower() in rustList:
+		makeNewRust(projName)
+	else:
+		print("ERROR: A project type with that name does not exist")
 
 
 if (__name__ == "__main__"):
-    if(len(argv) < 2):
-        makeNew()
-    else:
-        parseArgv()
-
+	if(len(argv) < 2):
+		makeNew()
+	else:
+		parseArgv()
